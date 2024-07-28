d5bbac5006e8d0a98ba56c20f2f764977fa7f1bc
Santoshshriwas <santoshdhobi9589@gmail.com>
2024-07-28 16:14:07 +0530

    Tik tok game Complate

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+import React from 'react'
+import TicTavToe from './components/TicTavToe'
+
+const App = () => {
+  return (
+   <>
+   <h1>Tic Tok Tec Game</h1>
+    <TicTavToe/>
+   </>
+  )
+}
+
+export default App
+
